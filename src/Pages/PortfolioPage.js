a090b43a522b9c1bfcd05a379ae83b62ca65ee82
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-31 03:01:57 -0400

    cards are now stacked and side by side. still need to fix styling and add links to github and deployed site and fix overall styling

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 
 import Portfolio from '../components/Portfolio'
 import projects from "../projects.json";
+import Wrapper from '../components/Wrapper';
 
 
 class PortfolioPage extends Component {
 
     state = {
         projects
-      };
+    };
 
 render () {
 return (
-    <main>
-        <div className ="grid-container">
+    <Wrapper>
         {projects.map(project => (
             <Portfolio
-            key={project.id}
-            id={project.id}
-            picture={project.image}
-            name={project.name}
-            description={project.description}
-            tech={project.tech}
-            github={project.github}
-            site={project.site}
+                key={project.id}
+                id={project.id}
+                picture={project.image}
+                name={project.name}
+                description={project.description}
+                tech={project.tech}
+                github={project.github}
+                site={project.site}
             />
-            ))}
-        </div>
-    </main>
+        ))}
+    </Wrapper>
   );
 }
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,46 +1,25 @@
 import React from "react";
 import {Card, Button, CardImg, CardTitle, CardText,CardBody
 } from 'reactstrap';
-import Wrapper from "../components/Wrapper";
 
 
 function Projects(props) {
   return (
-    <div className="featured-projects">
-      <div className="row">
-        <div className="col col-md-6">
-          <Wrapper>
-          <Card>
-            <Button id="project-button" block>{props.name}</Button>
-            <CardImg top width="100%" src={props.picture} alt="website image"/>
-            <CardBody>
+    <div className="card">
+      <div className="img-container">
+        <Card>
+          <Button id="project-button" block>{props.name}</Button>
+          <CardImg top width="100%" src={props.picture} alt="website image"/>
+            <CardBody className="content">
               <CardTitle style={{fontSize: "18px", fontWeight: "bold"}}>DESCRIPTION:</CardTitle>
               <CardText style={{fontSize:"18px"}}> {props.description} </CardText>
               <CardTitle style={{fontSize: "18px", fontWeight: "bold"}}>TECHNOLOGIES USED:</CardTitle>
               <CardText> {props.tech }</CardText>
-              <CardTitle style={{fontSize: "18px", fontWeight: "bold", color:"#1F8832"}}> GITHUB | DEPLOYED SITE</CardTitle>
+              <CardTitle style={{fontSize: "18px", fontWeight: "bold", color:"#1F8832", textAlign:"center"}}> GITHUB | DEPLOYED SITE</CardTitle>
             </CardBody>
-          </Card>
-          </Wrapper>
-        </div>
-        
-     
-        {/* <div className="col col-md-6">
-          <Card>
-            <Button id="project-button" block>{props.name}</Button>
-            <CardImg top width="100%" src={props.picture} alt="website image"/>
-            <CardBody>
-            <CardTitle style={{fontSize: "18px", fontWeight: "bold"}}>DESCRIPTION:</CardTitle>
-            <CardText style={{fontSize:"18px"}}>{props.description }</CardText>
-              <CardTitle style={{fontSize: "18px", fontWeight: "bold"}}>TECHNOLOGIES USED:</CardTitle>
-              <CardText style={{fontSize:"18px"}}> {props.tech} </CardText>
-              <CardTitle style={{fontSize: "18px", fontWeight: "bold", color:"#1F8832"}}> GITHUB | DEPLOYED SITE</CardTitle>
-            </CardBody>
-          </Card>
-        </div> */}
-
+        </Card>
       </div>
-      </div> 
+    </div> 
   );
 };
   
